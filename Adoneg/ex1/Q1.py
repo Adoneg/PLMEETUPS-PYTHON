b32a229a41e9198d2c4ad22b6a7bd629ede634a3
Adoneg <victoradoneg2@gmail.com>
2022-10-04 22:04:04 +0100

    code for exercise 1

--- Adoneg/ex1/Q1.py
+++ Adoneg/ex1/Q1.py
@@ -2,9 +2,9 @@
  # being their class number and print out the first student and his/her class number
 
 class_dict = {
-     'miriam': 1,
-     'shammah': 2,
-     'uyaka': 3,
+     'Miriam': 1,
+     'Shammah': 2,
+     'Uyaka': 3,
      'Emma': 4
     }
 names_of_students = []
@@ -12,17 +12,6 @@ for key, value in class_dict.items():
     names_of_students.append(key)
 
 first_student = names_of_students[0]
-first_student_class_num = class_dict['miriam']
+first_student_class_num = class_dict['Miriam']
 
 print(f'first_student:{first_student}\n class number:{first_student_class_num}')
-
-# Q2. Create two intergers containing any numbers and add them and print the result
-
-num1 = 3
-num2 = 4
-sum = num1 + num2
-print(f'sum={sum}')
-
-# Q3. create a list containing 1 interger, 1 string, 1 float and  1 boolean
-
-list = [1, "Adoneg", 12.45, True]
---
+++ Adoneg/ex1/Q2.py
@@ -0,0 +1,6 @@
+# Q2. Create two intergers containing any numbers and add them and print the result
+
+num1 = 3
+num2 = 4
+sum = num1 + num2
+print(f'sum={sum}')
